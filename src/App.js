349c2633a27637b42b6eb0be6b3a13240c14c9bc
apatel401 <51169932+apatel401@users.noreply.github.com>
2022-07-19 16:22:22 -0400

    Creating all files required for dashboard

--- src/App.js
+++ src/App.js
@@ -21,10 +21,45 @@ const App = () => {
             </TooltipComponent>
         </div>
         {activeMenu ? 
-        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white" >Sidebar full</div> : 
-        <div className="w-0 sidebar dark:bg-secondary-dark-bg">Sidebar 0</div>}
-        <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2' } `} >
+        <div 
+        className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white" >
+        Sidebar full
+        </div> : 
+        <div 
+        className="w-0 sidebar dark:bg-secondary-dark-bg">Sidebar 0</div>}
+        <div 
+        className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2' } `} >
+          <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+            Navbar
+          </div>
     </div>
+          <div>
+          <Routes>
+            {/* Dashboard  */}
+            <Route path="/" element="Ecommerce"/>
+            <Route path="/ecommerce" element="Ecommerce"/>
+
+            {/* Pages  */}
+            <Route path="/orders" element="Orders"/>
+            <Route path="/employees" element="Employees"/>
+            <Route path="/customers" element="Customers"/>
+
+            {/* Apps  */}
+            <Route path="/kanban" element="Kanban"/>
+            <Route path="/editor" element="Editor"/>
+            <Route path="/calender" element="Calender"/>
+            <Route path="/color-picker" element="ColorPicker"/>
+
+            {/* charts  */}
+            <Route path="/line" element="Line"/>
+            <Route path="/area" element="Editor"/>
+            <Route path="/pie" element="Pie"/>
+            <Route path="/financial" element="Financial"/>
+            <Route path="/color-mapping" element="ColorMapping"/>
+            <Route path="/pyramid" element="Pyramid"/>
+            <Route path="/stacked" element="Stacked"/>
+          </Routes>
+          </div>
     </div>
     </BrowserRouter>
    </div>
