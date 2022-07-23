055efc2b2c76d48adfc6f507605d6ce9f9d1a897
apatel401 <51169932+apatel401@users.noreply.github.com>
2022-07-22 23:02:05 -0400

    sidebar responsive fixing mobile issue

--- src/App.js
+++ src/App.js
@@ -62,7 +62,6 @@ const App = () => {
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
-          </div>
           <div>
             <Routes>
               {/* Dashboard  */}
@@ -92,6 +91,7 @@ const App = () => {
               <Route path="/stacked" element={<Stacked />} />
             </Routes>
           </div>
+        </div>
         </div>
       </BrowserRouter>
     </div>
