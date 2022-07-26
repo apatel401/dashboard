c0a18fab5e722d97b08a20539f6bbe10df45b937
apatel401 <51169932+apatel401@users.noreply.github.com>
2022-07-26 15:44:40 -0400

    color picker setup

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Header } from "../components";
 
+import {
+  HtmlEditor,
+  RichTextEditorComponent,
+  QuickToolbar,
+  Image,
+  Inject,
+  Link,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+
+import { EditorData } from "../data/dummy";
 const Editor = () => {
   return (
-    <div>Editor</div>
-  )
-}
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Editor" />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[QuickToolbar, Image, HtmlEditor, Link, Toolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
+};
 
-export default Editor
+export default Editor;
